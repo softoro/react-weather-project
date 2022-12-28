25896db63cccc3c51a156433427722746641a5d5
softoro <112343085+softoro@users.noreply.github.com>
2022-12-28 19:38:31 +0100

    Set up app structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="London" />
+
+        <footer>
+          This project was coded by Sofia Torosian and is open-sourced on{" "}
+          <a
+            href="https://github.com/softoro/react-weather-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://clinquant-strudel-e966be.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
